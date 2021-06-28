406b08acab2ff0e3aaed0726c72962227a87f7b6
spg1101 <sharping@SharPingui-MacBookPro.local>
2021-06-28 19:20:31 +0900

    m

---
+++ hw2.py
@@ -0,0 +1,13 @@
+﻿inch = float(input('inch 입력  :  '))
+cm = inch * 2.54
+
+number = '201401793'
+name = 'JiHoon Shim'
+
+print('학번  :  %20s' %number)
+print('성명  :  %20s' %name)
+
+print('='*30)
+print(' '*5 + '%s inch' %'{:08.2f}'.format(inch))
+print(' '*5 + '%s cm' %'{:08.2f}'.format(cm))
+print('='*30)
